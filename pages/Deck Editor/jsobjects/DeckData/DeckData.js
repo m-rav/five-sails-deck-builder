dde765c2a2cdbbeb24fee6b7e9ca2335c1e91a68
m-rav <m-rav>
2023-12-30 23:28:42 +0000

    System generated commit, initial commit

---
+++ pages/Deck Editor/jsobjects/DeckData/DeckData.js
@@ -0,0 +1,25 @@
+export default {
+	deck_list: [],
+	getCurrentFaction () {
+		var currentFaction;
+		for(var faction of (GetFactionMetaDeta.data)) {
+			if (faction.playable) {
+				currentFaction = faction;
+			}
+			console.log(faction);
+			console.log(faction.faction_name === appsmith.URL.queryParams.faction);
+		}
+		return currentFaction;
+	},
+	currentFaction: this.getCurrentFaction()
+	// allFactions : GetFactionMetaDeta.data,
+	// currentFaction : CurrentFaction.data[0],
+	// allCardsCore: GetCardsCore.data,
+	// updateDeck () {
+		// //	write code here
+		// //	this.myVar1 = [1,2,3]
+		//deck_list{{tbl_customerInfo.triggeredRow}}
+		// 
+		// 
+	// },
+}
