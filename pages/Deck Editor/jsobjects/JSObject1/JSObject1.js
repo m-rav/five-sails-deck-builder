2a7fab5f5001d58c3150a0bd630f005f2cc29b93
m-rav <m-rav>
2024-01-01 08:08:34 +0000

    mhm

--- pages/Deck Editor/jsobjects/JSObject1/JSObject1.js
+++ pages/Deck Editor/jsobjects/JSObject1/JSObject1.js
@@ -1,16 +1,61 @@
 export default {
-	deck_list: [],
+	defaultFaction: "Castille",
+	async getDefaultDeck() {
+		let faction = this.determineFaction(this.defaultFaction);
+		const result = await GetAvailableFactionCardsCopy.run(faction);
+		return {
+			"faction": faction,
+			"availableFactionCards": result,
+			"deckList": []
+		};
+	},
+	determineFaction(factionName) {
+		console.log(factionName);
+		const factionData = GetFactionMetaData.data;
+		for (var i = 0; i < factionData.length; i++) {
+			let faction = factionData[i];
+			if (faction.faction_name === factionName) return faction;
+		}
+		throw new Error("No Faction with name " + factionName);
+	},
+	parseUrlData() {
+		let hasDeckId = appsmith.URL.queryParams.hasOwnProperty('deckId');
+		if (hasDeckId) {
+			const savedDeckId = parseInt(appsmith.URL.queryParams.deckId);
+			const savedDeckData = GetDeckById.run(savedDeckId);
+		}
+		let hasDeckList = appsmith.URL.queryParams.hasOwnProperty('deckList');
+		if (hasDeckList) {
+			try{
+				const deckDataEncoded = appsmith.URL.queryParams.deckList;
+				const deckDataDecoded = JSON.parse(atob(deckDataEncoded));
+				const faction = this.determineFaction(deckDataDecoded.f);
+				return {
+						faction:faction,
+						ids: [1],
+						qtys: [1]
+				};
+			}
+			catch(error){ 
+				console.log(error);
+			}
+		}
+		return this.getDefaultDeck();
+	},
 	setExportData() {
-		let decodedExport = {f:0, ids:[1,19,125,200],qtys:[1,0,0,1]};
+		let decodedExport = {f:0, ids:[1,19,125,200,10,38,20,21],qtys:[1,0,0,1]};
 		let encodedExport = btoa(JSON.stringify((decodedExport)));
-		let returnPath = appsmith.URL.pathname + "?deckList=" + encodedExport;
+		let returnPath = appsmith.URL.protocol + "//" + appsmith.URL.host + appsmith.URL.pathname + "?deckList=" + encodedExport;
 		console.log(JSON.stringify(decodedExport));
+		console.log({"path-host": appsmith.URL.host});
+		console.log({"path-protocol": appsmith.URL.protocol});
+		console.log({"path": appsmith.URL.hostname});
 		console.log({"path": appsmith.URL.fullPath});
+		
 		console.log({"path": returnPath});
 		export_string_textbox.setValue(returnPath)
 		return returnPath;
-  },
-	copyExportData: () => copyToClipboard(export_string_textbox.text),
+  }
 	// allFactions : GetFactionMetaDeta.data,
 	// currentFaction : CurrentFaction.data[0],
 	// allCardsCore: GetCardsCore.data,
@@ -21,5 +66,4 @@ export default {
 		// 
 		// 
 	// },
-	
 }
